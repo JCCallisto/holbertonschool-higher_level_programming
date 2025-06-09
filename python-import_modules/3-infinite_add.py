234921c1921e18d32472a37aaa618f52af65826c
JCCallisto <jeancarrion8@gmail.com>
2025-06-09 12:06:40 -0400

    Python import modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    
+
     import sys
-    
+
     argv = sys.argv[1:]
     result = 0
-    
+
     for arg in argv:
         result += int(arg)
-    
+
     print(result)
