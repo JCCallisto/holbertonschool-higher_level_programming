ccda7fa47ab1f3ca0095a41cf38b3c4ef7a2bdcd
JCCallisto <jeancarrion8@gmail.com>
2025-06-12 11:26:45 -0400

    Python data structures

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replace an element in a list at a specific position 
+    """Replace an element in a list at a specific position
     without modifying the original list"""
     new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
