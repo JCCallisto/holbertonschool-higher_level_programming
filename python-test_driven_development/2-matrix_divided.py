5ad2e99c713a71d025942a8aa00807f4df068522
JCCallisto <jeancarrion8@gmail.com>
2025-06-17 11:23:51 -0400

    Python test driven development

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -35,7 +35,7 @@ def matrix_divided(matrix, div):
         [[5.0, 10.0], [15.0, 20.0]]
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix"
+        raise TypeError("matrix must be a matrix "
         "(list of lists) of integers/floats")
 
     if not matrix:
@@ -44,19 +44,19 @@ def matrix_divided(matrix, div):
 
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix"
+            raise TypeError("matrix must be a matrix "
             "(list of lists) of integers/floats")
 
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
-            raise TypeError("Each row of the matrix" \
+            raise TypeError("Each row of the matrix "
             "must have the same size")
 
     for row in matrix:
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix"
+                raise TypeError("matrix must be a matrix "
                 "(list of lists) of integers/floats")
 
     if not isinstance(div, (int, float)):
