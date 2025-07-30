4206e7a32285ed2a20afbd33902dae21f5e2c635
JCCallisto <jeancarrion8@gmail.com>
2025-07-29 20:43:30 -0400

    Restful api

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -20,6 +20,10 @@ def verify_password(username, password):
     if username in users and check_password_hash(users[username]['password'], password):
         return username
     return None
+
+@auth.error_handler
+def unauthorized():
+    return jsonify({"error": "Unauthorized access"}), 401
 
 @jwt.unauthorized_loader
 def handle_unauthorized_error(err):
@@ -85,7 +89,6 @@ def admin_only():
 
     if user_role != 'admin':
         return jsonify({"error": "Admin access required"}), 403
-    
     return "Admin Access: Granted"
 
 if __name__ == '__main__':
